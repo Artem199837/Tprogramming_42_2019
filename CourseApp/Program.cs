9eebb5005e261400ce98c137fc3f865604d287b3
Александр <@DESKTOP-6G1MCJ8>
2019-10-20 19:32:48 +0300

    Задание A и B сделаны

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,26 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CourseApp
 {
     public class Program
     {
-        public static double MyFunction(double a, double b, double x)
+        public static double FuncthionZnach(double x)
         {
-            var y = (a * Math.Pow(x, 2)) + (b * x);
-            return y;
+            double a = 1.2;
+            double b = 0.28;
+            double result = (Math.Pow(b, 3) + Math.Pow(Math.Sin(a * x), 2)) / (Math.Asin(x * b * x) + Math.Exp(-x / 2));
+            return result;
         }
 
-        public static double[] TaskA(double a, double b, double xn, double xk, double dx)
+        public static double[] FuncthionForShag(double x_natch, double x_konch, double x_shag)
         {
-            return new double[0];
+            var result = new double[(int)((x_konch - x_natch) / x_shag) + 1];
+            int kolch = 0;
+            for (double i = x_natch; i <= x_konch; i += x_shag)
+            {
+                result[kolch] = FuncthionZnach(i);
+                kolch++;
+            }
+
+            return result;
         }
 
-        public static double[] TaskB(double a, double b, double[] x)
+        public static double[] FuncthionForMass(double[] x)
         {
             var y = new double[x.Length];
             for (var i = 0; i < x.Length; i++)
             {
-                y[i] = MyFunction(a, b, x[i]);
+                y[i] = FuncthionZnach(x[i]);
             }
 
             return y;
@@ -28,18 +42,20 @@ namespace CourseApp
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            const double a = 2.2;
-            const double b = 3.8;
-            var resSingle = MyFunction(a, b, 4);
-            Console.WriteLine(resSingle);
-            var x = new double[] { 1, 2, 3, 4, 5 };
-            var taskBRes = TaskB(a, b, x);
-            foreach (var item in taskBRes)
+            Console.WriteLine($"Задание A:");
+            foreach (double element in FuncthionForShag(0.7, 2.2, 0.3))
             {
-                Console.WriteLine($"y = {item}");
+                Console.WriteLine(element);
             }
 
+            Console.WriteLine($"\nЗадание B:");
+            var x = new double[] { 0.25, 0.36, 0.56, 0.94, 1.28 };
+            foreach (var element in FuncthionForMass(x))
+            {
+                Console.WriteLine($"y = {element}");
+            }
+
+            Console.WriteLine();
             Console.ReadLine();
         }
     }
